0ea10aaec7ffc65a5442d7b884e096bf00b18614
Simone Mordue <57173377+simone-mordue@users.noreply.github.com>
2021-03-24 11:45:42 +0000

    delete

--- SDGclassy-master/project-simone/infer-scores_simone.sh
+++ SDGclassy-master/project-simone/infer-scores_simone.sh
@@ -9,8 +9,8 @@ f='full_texts_test'                      			#select target files
 cd "${d}" 
 rm "${d}"/"${p}"/"${f}"/.DS_Store || true   #Clean up. We ignore any errors here
 
-#Import target data
-mallet/bin/mallet import-dir --input "${d}"/"${p}"/"${f}"  --output "${d}"/"${p}"/workshop/inferring-"${f}".mallet --keep-sequence --remove-stopwords --extra-stopwords "${d}"/workshop/extra-exclude-words_new.txt --keep-sequence-bigrams --gram-sizes 1,2  --use-pipe-from "${d}"/classifier/results/"${c}".mallet
+#Import target data and create inferencer
+mallet/bin/mallet import-dir --input "${d}"/"${p}"/"${f}"  --output "${d}"/"${p}"/workshop/inferring-"${f}".mallet --keep-sequence --remove-stopwords --extra-stopwords "${d}"/"${p}"/workshop/extra-exclude-words_new.txt --keep-sequence-bigrams --gram-sizes 1,2  --use-pipe-from "${d}"/classifier/results/"${c}".mallet
 
 #Apply topic model and get SDG scores
 mallet/bin/mallet infer-topics --input "${d}"/"${p}"/workshop/inferring-"${f}".mallet --inferencer "${d}"/classifier/results/"${c}"-inferencer.mallet --output-doc-topics "${d}"/"${p}"/output/scores-"${f}"-"${c}".txt   
